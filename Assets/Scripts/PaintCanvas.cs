ddbefd408a0c536b9a3fdb7b874a4580ac498e59
georgeHadjitofallis <hadjitg@gmail.com>
2019-05-26 14:22:48 +0100

    start

---
+++ Assets/Scripts/PaintCanvas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PaintCanvas : MonoBehaviour
+{
+    public static Texture2D Texture { get; private set; }
+
+
+    public static byte[] GetAllTextureData()
+    {
+        return Texture.GetRawTextureData();
+    }
+
+    private void Start()
+    {
+        PrepareTemporaryTexture();
+        float quadHeight = Camera.main.orthographicSize * 2.0f;
+        float quadWidth = quadHeight * Screen.width / Screen.height;
+        transform.localScale = new Vector3(quadWidth, quadHeight, 1);
+    }
+
+
+    private void PrepareTemporaryTexture()
+    {
+        Texture = (Texture2D)GameObject.Instantiate(GetComponent<Renderer>().material.mainTexture);
+        GetComponent<Renderer>().material.mainTexture = Texture;
+    }
+}
---
+++ Assets/Scripts/PaintManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PaintManager : MonoBehaviour
+{
+
+    void Start()
+    {
+    
+    }
+
+    void Update()
+    {
+        //Check if the left Mouse button is clicked
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Debug.Log("Pressed");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("HIT");
+                var pallet = hit.collider.GetComponent<PaintCanvas>();
+                if (pallet != null)
+                {
+                    Debug.Log(hit.textureCoord);
+                    Debug.Log(hit.point);
+
+                    Renderer rend = hit.transform.GetComponent<Renderer>();
+                    MeshCollider meshCollider = hit.collider as MeshCollider;
+
+                    if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
+                        return;
+
+                    Texture2D tex = rend.material.mainTexture as Texture2D;
+                    Vector2 pixelUV = hit.textureCoord;
+                    pixelUV.x *= tex.width;
+                    pixelUV.y *= tex.height;
+
+                    BrushAreaWithColor(pixelUV, Color.black, 1);
+                }
+            }
+        }
+    }
+
+    private void BrushAreaWithColor(Vector2 pixelUV, Color color, int size)
+    {
+        for (int x = -size; x < size; x++)
+        {
+            for (int y = -size; y < size; y++)
+            {
+                PaintCanvas.Texture.SetPixel((int)pixelUV.x + x, (int)pixelUV.y + y, color);
+            }
+        }
+
+        PaintCanvas.Texture.Apply();
+    }
+}
